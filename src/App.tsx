94661f2b040e7446ac85c9a927cecbc46c6a98ea
Orlando Gomes <orlandosjm@yahoo.com.br>
2023-01-25 18:18:28 -0300

    add html structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,69 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-        <h1>Hello world!</h1>
+    <div>
+        <header>todo</header>
+        <main>
+          <div className="new-task">
+            <input />
+            <button>Criar</button>
+          </div>
+
+          <div className="tasks">
+            <div className='info'>
+              <div>Tarefas criadas 5</div>
+              <div>Concluídas 2 de 5</div>
+            </div>
+
+            <div className='list'>
+              
+              <div className='task'>
+                <input type='checkbox' />
+                <input 
+                  type='text' 
+                  value='Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.' 
+                />
+                <button>excluir</button>
+              </div>
+
+              <div className='task'>
+                <input type='checkbox' />
+                <input 
+                  type='text' 
+                  value='Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.' 
+                />
+                <button>excluir</button>
+              </div>
+
+              <div className='task'>
+                <input type='checkbox' />
+                <input 
+                  type='text' 
+                  value='Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.' 
+                />
+                <button>excluir</button>
+              </div>
+
+              <div className='task'>
+                <input type='checkbox' />
+                <input 
+                  type='text' 
+                  value='Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.' 
+                />
+                <button>excluir</button>
+              </div>
+
+              <div className='task'>
+                <input type='checkbox' />
+                <input 
+                  type='text' 
+                  value='Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.' 
+                />
+                <button>excluir</button>
+              </div>
+            </div>
+          </div>
+        </main>
     </div>
   )
 }
